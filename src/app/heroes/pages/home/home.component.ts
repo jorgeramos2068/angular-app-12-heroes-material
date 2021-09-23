111f2a6ec27e6b195b329f4f2ac8a4d64eb6e8fa
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-23 16:19:34 -0500

    Implement Material components for heroes home page

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -3,14 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
