c6713a5f824dabec6ced383990bb074a86aa63ee
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-28 12:44:06 -0500

    Implement functionality to edit heroes

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -21,18 +23,41 @@ export class AddComponent implements OnInit {
     { id: 'Marvel Comics', desc: 'Marvel Comics' },
   ];
 
-  constructor(private heroesService: HeroesService) {}
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHero(id)))
+      .subscribe({
+        next: (hero) => {
+          this.hero = hero;
+        },
+      });
+  }
 
   submit(): void {
     if (this.hero.superhero.trim().length === 0) {
       return;
     }
-    this.heroesService.saveHero(this.hero).subscribe({
-      next: (resp) => {
-        console.log(resp);
-      },
-    });
+    if (this.hero.id) {
+      // Update
+      this.heroesService.updateHero(this.hero).subscribe({
+        next: (resp) => {
+          console.log('Updated:', resp);
+        },
+      });
+    } else {
+      // Create
+      this.heroesService.saveHero(this.hero).subscribe({
+        next: (hero) => {
+          this.router.navigate(['/heroes/edit', hero.id]);
+          console.log('Created:', hero);
+        },
+      });
+    }
   }
 }
