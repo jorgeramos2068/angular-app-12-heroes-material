a431ed15b5a387b49119d98b0b33da098abd11ba
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-29 10:34:48 -0500

    Add dialog and snackbar functionalities for heroes form

---
+++ src/app/heroes/components/confirm/confirm.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+})
+export class ConfirmComponent implements OnInit {
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Hero
+  ) {}
+
+  ngOnInit(): void {}
+
+  confirmDeletion(): void {
+    this.dialogRef.close(true);
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
