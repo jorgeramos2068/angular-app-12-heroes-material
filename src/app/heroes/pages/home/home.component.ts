65321e06cef8336ec0a1f89842727224272f489f
Jorge Ramos <jose.ramos@nextiva.com>
2021-10-01 10:09:06 -0500

    Create getter to get user data

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
+
+  get auth(): Auth {
+    return this.authService.auth;
+  }
 
   ngOnInit(): void {}
 
