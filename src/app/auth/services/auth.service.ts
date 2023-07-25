eb486a99800511f2a07836127444c13784f55cc1
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 21:57:38 -0600

    Create getter to get user data

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -9,10 +10,17 @@ import { Auth } from '../interfaces/auth.interface';
 })
 export class AuthService {
   private baseUrl: string = environment.endpointBaseUrl;
+  private _auth: Auth | undefined;
 
   constructor(private http: HttpClient) {}
 
+  get auth(): Auth {
+    return { ...this._auth! };
+  }
+
   login(): Observable<Auth> {
-    return this.http.get<Auth>(`${this.baseUrl}/users/1`);
+    return this.http
+      .get<Auth>(`${this.baseUrl}/users/1`)
+      .pipe(tap((auth) => (this._auth = auth)));
   }
 }
